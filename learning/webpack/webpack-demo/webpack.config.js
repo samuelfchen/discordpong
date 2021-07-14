0b6ff6a2ed1aa30554f243b9a13347c59674a5f1
Samuel Chen <samuel.f.chen@gmail.com>
2021-07-14 16:14:48 +1000

    done with code splitting

--- learning/webpack/webpack-demo/webpack.config.js
+++ learning/webpack/webpack-demo/webpack.config.js
@@ -5,21 +5,9 @@ module.exports = {
     mode: 'development',
     entry: {
         index: './src/index.js',
-        print: './src/print.js'
     },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Development',
-        }),
-    ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true,
-        publicPath: '/',
     },
 };
